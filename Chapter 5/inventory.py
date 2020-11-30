3db8f48e6ce11c250824b85638f1afd4215cf20a
afinedrizzle <58958990+afinedrizzle@users.noreply.github.com>
2020-11-30 01:00:26 +0100

    Chapter 5 -
    Program to display items and add items to an inventory.

---
+++ Chapter 5/inventory.py
@@ -0,0 +1,34 @@
+#Program to 
+
+stuff = {'arrow': 12,
+         'gold coin': 42,
+         'rope': 1,
+         'torch': 6,
+         'dagger': 1}
+
+def displayInventory(inventory):
+    print('Inventory: ')
+    total = 0
+    for k, v in inventory.items():
+        print(str(v) + ' ' + k)
+        total += v
+    print('Total number of items: ' + str(total))
+
+# displayInventory(stuff)
+
+def addToInventory(inventory, addedItems):
+    for k in addedItems:
+        if k not in inventory:
+            inventory.setdefault(k, 1)
+        else:
+            inventory[k] += 1
+    return inventory
+
+loot = {'gold coin': 42, 'rope': 1, 'pearl': 5}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby', 'gold coin']
+
+
+loot = addToInventory(loot, dragonLoot)
+displayInventory(loot)
+    
+       
