433451215a4b95f8da27fbe68c68213b3d861b21
Marcin Małek <58958990+MarcinTomasz@users.noreply.github.com>
2021-03-19 02:41:50 +0100

    Added solution to practice project.

--- Chapter 10/DebuggingCoinToss.py
+++ Chapter 10/DebuggingCoinToss.py
@@ -7,6 +7,13 @@ while guess not in ('heads', 'tails'):
   print('Guess the coin toss! Enter heads or tails:')
   guess = input()
 toss = random.randint(0, 1) # 0 is tails, 1 is heads
+
+#lines 12-15 are my solution to the problem.
+if toss == 1:
+    toss = 'heads'
+else:
+    toss = 'tails'
+    
 if toss == guess:
   print('You got it!')
  else:
