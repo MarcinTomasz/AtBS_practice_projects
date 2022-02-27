80f9d144592e33f122918124f4bb2d32a98b76a6
Marcin Małek <58958990+MarcinTomasz@users.noreply.github.com>
2022-02-27 01:53:26 +0100

    added line to be able to open Firefox browser

--- Chapter 11/controlwebbrowser.py
+++ Chapter 11/controlwebbrowser.py
@@ -1,8 +1,14 @@
-#First script trying Selenium.
+#Script to open web browser with Selenium
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
-PATH = "C:\\Users\\novyp\\Desktop\\Python\\chromedriver.exe"
-
+''' #Open Chrome
+PATH = "C:\\Users\\novyp\\Desktop\\Python\\Webdrivers\\chromedriver.exe"
 browser = webdriver.Chrome(PATH)
+browser.get('http://inventwithpython.com') '''
 
-browser.get('http://inventwithpython.com')
+#Open Firefox
+options = Options()
+options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+driver = webdriver.Firefox(options=options)
+driver.get('http://inventwithpython.com/')
