47ce3bdff21698b6062fe2acecdbc1b43d7d29a9
Marcin Małek <58958990+MarcinTomasz@users.noreply.github.com>
2022-02-28 03:55:09 +0100

    Added find_elements code

--- Chapter 11/controlwebbrowser.py
+++ Chapter 11/controlwebbrowser.py
@@ -1,5 +1,6 @@
 #Script to open web browser with Selenium
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 
 ''' #Open Chrome
@@ -12,3 +13,11 @@ options = Options()
 options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
 driver = webdriver.Firefox(options=options)
 driver.get('http://inventwithpython.com/')
+
+try:
+    elem = driver.find_elements(By.TAG_NAME, 'p')
+    print('Found the element with that class name!')
+    for e in elem:
+        print(e.text)
+except:
+    print('Was not able to find an element with that name.')
