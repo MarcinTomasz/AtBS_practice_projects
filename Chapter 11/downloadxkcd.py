aa0d37fc931e514ce5f5f3c9a273b8b0b4e04be4
Marcin Małek <58958990+MarcinTomasz@users.noreply.github.com>
2022-03-13 22:13:27 +0100

    Update downloadxkcd.py
    Removed unnecesary code block.

--- Chapter 11/downloadxkcd.py
+++ Chapter 11/downloadxkcd.py
@@ -3,7 +3,7 @@
 
 import requests, os, bs4
 
-url = 'http://xkcd.com'                 #starting url
+url = 'https://xkcd.com/25'             #starting url
 os.makedirs('xkcd', exist_ok=True)      #store comics in ./xkcd, folder exist_ok=True prevents exception if folder already exists
 
 while not url.endswith('#'):
@@ -13,27 +13,22 @@ while not url.endswith('#'):
   res.raise_for_status()                #Throws an exception and ends the program if something goes wrong with the download.
   
   #Creates BeautifulSoup text object from the res variable.
-  soup = bs4.BeautifulSoup(res.text)    
+  soup = bs4.BeautifulSoup(res.text, features= "html.parser")   
   
   #Find the URL of the comic image.
   comicElem = soup.select('#comic img')
   if comicElem == []:
     print('Could not find comic image.')
+    
   else:
-    try:                               #Needed because some urls are missing domain name.
+    try:
         comicUrl = 'https:' + comicElem[0].get('src')
             
         #Download the image.
         print('Downloading image %s...' % (comicUrl))
         res = requests.get(comicUrl)
         res.raise_for_status()
-    
-        #Save the image to ./xkcd.
-        imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
-        for chunk in res.iter_content(100000):
-            imageFile.write(chunk)
-        imageFile.close()
-    
+     
     except:
         comicUrl = 'https://xkcd.com' + comicElem[0].get('src')
             
@@ -42,14 +37,16 @@ while not url.endswith('#'):
         res = requests.get(comicUrl)
         res.raise_for_status()
     
+    finally:
         #Save the image to ./xkcd.
         imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
         for chunk in res.iter_content(100000):
-            imageFile.write(chunk)
+          imageFile.write(chunk)
         imageFile.close()
+        
   
   #Get the Prev button's url.
   prevLink = soup.select('a[rel="prev"]')[0]
-  url = 'http://xkcd.com' + prevLink.get('href')
+  url = 'https://xkcd.com' + prevLink.get('href')
     
 print('Done')
