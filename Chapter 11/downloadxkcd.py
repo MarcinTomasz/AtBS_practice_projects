d08f810dfb9de1a7be3b3dbb4606b6b216cae664
Marcin Małek <58958990+MarcinTomasz@users.noreply.github.com>
2022-03-16 02:18:57 +0100

    Update downloadxkcd.py

--- Chapter 11/downloadxkcd.py
+++ Chapter 11/downloadxkcd.py
@@ -1,42 +1,40 @@
 #! python3
 
-#Program to download images from photo hosting website with certain tag
+#Program to download all XKCD comics
 
 import os, requests, bs4
 
-searchterm = input('Enter search term(s): ')
+url = 'https://xkcd.com/'
+os.makedirs('xkcd', exist_ok = True)
 
-def image_downloader(extension):
-    """Search and download all images from Imgur."""
-    url = 'https://imgur.com/search?q=' + searchterm
-    os.makedirs('/Users/novyp/Desktop/pics', exist_ok=True)
-    
-    res = requests.get(url)
-    res.raise_for_status()
-    
-    soup = bs4.BeautifulSoup(res.text, features= 'html.parser')   
-    image_elem = soup.select('.post > .image-list-link img') #select method is used to find matching elements using 
-                                                             #CSS selector - all image-list-link imgs one level in all post classes
-    
-    for i, image in enumerate(image_elem):
-        #Convert image URL from thumbnail size to fullsize version.
-        image_url_s = 'https:' + image_elem[i].get('src')
-        image_url = image_url_s[:-5] + '.jpeg'
-        
-        print('Downloading image {}'.format(image_url))
-        res = requests.get(image_url)
-        res.raise_for_status()
-        image_file = open(os.path.join(r'/Users/novyp/Desktop/pics', os.path.basename(image_url)), 'wb')
-        
-        for chunk in res.iter_content(1000000):
-            image_file.write(chunk)
-        image_file.close()
-    
-    return len(image_elem)
+while not url.endswith('#):
+                       #Download the comic.
+                       print('Downloading page %s...' %url)
+                       res = requests.get(url)
+                       res.raise_for_status()
+                       
+                       soup = bs4.BeautifulSoup(res.text)
+                       
+                       #Find the URL of the comic image.
+                       comicElem = soup.select('#comic img')
+                       if comicElem == []:
+                           print('Could not find comic image.')
+                       else:
+                           comicUrl = comicElem[0].get('src')
+                       #Download image.
+                       print('Downloading image %s...' % (comicUrl))
+                       res =requets.get(comicUrl)
+                       res.raise_for_status()
+                       
+                       #Save the image to ./xkcd.
+                       imageFile = open(os.path('skcd', os.path.basename(comicUrl)), 'wb')
+                       for chunk in res.iter_content(100000):
+                            imageFile.write(chunk)
+                       imageFile.close()
 
-downloaded = image_downloader('jpg')
-
-if downloaded == 0:
-    print('No images found.')
-else:
-    print('All ' + str(downloaded) + ' files successfully downloaded.')
+#Get the Previous button's url.
+prevLink = soup.select('a[rel = "prev"')[0]
+url = 'http://xkcd.com' + prevLink.get('href')
+                                    
+print('Done.')                    
+                    
