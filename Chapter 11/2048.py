e94ef8c09298a332d2ff07c038f6797fc41cc3b5
MTM <marcintim001@gmail.com>
2022-04-12 23:00:55 +0200

    Cleaned up some useless code.

--- Chapter 11/2048.py
+++ Chapter 11/2048.py
@@ -6,13 +6,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import bs4, requests
 
 #Arguments needed to suppress some certificate errors from appearing.
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--ignore-ssl-errors')
-driver = webdriver.Chrome(chrome_options=options)
 
 #Open 2048 Github page.
 PATH = "C:\\Users\\novyp\\Desktop\\Python\\Webdrivers\\chromedriver.exe"
@@ -27,27 +25,19 @@ time.sleep(1)
 
 #Click cookies.
 clickcookies = WebDriverWait(browser, 1).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="ez-accept-all"]'))).click()
-time.sleep(1)
 
 #Play game with arrows.
 playgame = browser.find_element(by= By.TAG_NAME, value= 'html')
-
-#Not sure if below code is ultimately useful.
-url = 'https://play2048.co/'
-res = requests.get(url)
-
-time.sleep(1)
-
 while True:
-    try:
+    # try:
         playgame.send_keys(Keys.ARROW_UP)
         playgame.send_keys(Keys.ARROW_DOWN)
         playgame.send_keys(Keys.ARROW_LEFT)
         playgame.send_keys(Keys.ARROW_RIGHT)
     #Still need to fix this part so window closes when game is over.
-    except:
-        soup = bs4.BeautifulSoup(res.text, features= 'html.parser')
-        endofgame = soup.select('#game-message game-over > #retry-button')
-        endofgame.click()
+    # except:
+    #     soup = bs4.BeautifulSoup(res.text, features= 'html.parser')
+    #     endofgame = soup.select('#game-message game-over > #retry-button')
+    #     endofgame.click()
         
 time.sleep(5)
