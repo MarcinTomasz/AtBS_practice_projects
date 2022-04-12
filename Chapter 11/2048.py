b01d6248a4a67f589d665d743e8f9a6187c95702
MTM <marcintim001@gmail.com>
2022-04-12 22:38:02 +0200

    Added code to run until game is filled.

--- Chapter 11/2048.py
+++ Chapter 11/2048.py
@@ -8,6 +8,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import bs4, requests
 
+#Arguments needed to suppress some certificate errors from appearing.
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+driver = webdriver.Chrome(chrome_options=options)
+
 #Open 2048 Github page.
 PATH = "C:\\Users\\novyp\\Desktop\\Python\\Webdrivers\\chromedriver.exe"
 browser = webdriver.Chrome(PATH)
@@ -26,20 +32,21 @@ time.sleep(1)
 #Play game with arrows.
 playgame = browser.find_element(by= By.TAG_NAME, value= 'html')
 
-
 #Not sure if below code is ultimately useful.
 url = 'https://play2048.co/'
 res = requests.get(url)
 
-soup = bs4.BeautifulSoup(res.text, features= 'html.parser')
-endofgame = soup.select('#game-message game-over > #retry-button')
-
-print(endofgame)
-print('Here is the message:' + endofgame)
-''' while not endofgame:
-    playgame.send_keys(Keys.ARROW_UP)
-    playgame.send_keys(Keys.ARROW_DOWN)
-    playgame.send_keys(Keys.ARROW_UP) '''
-
 time.sleep(1)
 
+while True:
+    try:
+        playgame.send_keys(Keys.ARROW_UP)
+        playgame.send_keys(Keys.ARROW_DOWN)
+        playgame.send_keys(Keys.ARROW_LEFT)
+        playgame.send_keys(Keys.ARROW_RIGHT)
+    except:
+        soup = bs4.BeautifulSoup(res.text, features= 'html.parser')
+        endofgame = soup.select('#game-message game-over > #retry-button')
+        endofgame.click()
+        
+time.sleep(5)
