137042d979d152360393a6a924c031d199f4171b
MTM <marcintim001@gmail.com>
2022-04-12 16:10:22 +0200

    Added line for user to enter number of comics to download

--- Chapter 11/downloadxkcd.py
+++ Chapter 11/downloadxkcd.py
@@ -3,7 +3,9 @@
 #Script to download all XKCD comics
 import os, requests, bs4
 
-url = 'https://xkcd.com/2508'
+number = input('How many comics do you want to download: ' )
+
+url = 'https://xkcd.com/' + number
 os.makedirs('xkcd', exist_ok = True)
 
 os.getcwd()
@@ -17,25 +19,25 @@ while not url.endswith('#'):
     soup = bs4.BeautifulSoup(res.text, features = 'html.parser')
     
     #Find the URL of the comic image.
-    comicElem = soup.select('#middleContainer')
+    comicElem = soup.select('#comic img')
     if comicElem == []:
         print('Could not find comic image.')
     else:
-        comicUrl = comicElem[0].get('a:nth-child(8)')
-    
+        comicUrl = 'https:' + comicElem[0].get('src')
+            
     #Download image.
     print('Downloading image %s...' % (comicUrl))
     res =requests.get(comicUrl)
     res.raise_for_status()
     
     #Save the image to ./xkcd.
-    imageFile = open(os.path('xkcd', os.path.basename('https:' + comicUrl)), 'wb')
+    imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
     for chunk in res.iter_content(100000):
         imageFile.write(chunk)
     imageFile.close()
 
     #Get the Previous button's url.
-    prevLink = soup.select('a[rel = "prev"')[0]
+    prevLink = soup.select('a[rel = "prev"]')[0]
     url = 'http://xkcd.com' + prevLink.get('href')
                                     
 print('All files downloaded.')             
