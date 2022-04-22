b9610f012dc1112adfaf34f7afef7beacb4fae89
MarcinTomasz <mtmalek@protonmail.com>
2022-04-22 02:10:54 +0200

    Added import file and read code.

--- Chapter 12/readCensusExcel.py
+++ Chapter 12/readCensusExcel.py
@@ -1,2 +1,16 @@
-#! python
-#Reading data from a spreadsheet
+#! python3
+#Tabulate population and number of census tracts for each country.
+
+import openpyxl, pprint
+print('Opening workbook...')
+wb = openpyxl.load_workbook('desktop/censuspopdata.xlsx')
+sheet = wb.get_sheet_by_name('Population by Census Tract')
+
+countyData = {}
+
+print('Reading rows...')
+for row in range(2, sheet.get_highest_row() + 1):
+    state = sheet['B' + str(row)].value
+    county = sheet['C' + str(row)].value
+    pop = sheet['D' + str(row)].value
+    
